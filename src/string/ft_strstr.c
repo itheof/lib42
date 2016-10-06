8876f65fc7855264fa1c661381f7427171bfc5a2
Clément Renault <renault.cle@gmail.com>
2016-10-06 12:44:29 +0200

    Norme strstr

--- src/string/ft_strstr.c
+++ src/string/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 12:14:45 by djean             #+#    #+#             */
-/*   Updated: 2016/10/05 01:34:02 by crenault         ###   ########.fr       */
+/*   Updated: 2016/10/06 12:42:43 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,18 @@ char			*ft_strstr(const char *big, const char *little)
 	big_len = ft_strlen(big);
 	little_len = ft_strlen(little);
 	init_skip_table(little, little_len, skip_table);
+	skip = 0;
 	if (little_len <= big_len)
-	{
-		skip = 0;
 		while (skip <= big_len - little_len)
 		{
 			i = little_len - 1;
 			while (little[i] == big[skip + i])
 			{
 				if (i == 0)
-				{
 					return ((char *)(uintptr_t)(big + skip));
-				}
 				--i;
 			}
 			skip += skip_table[(size_t)big[skip + little_len - 1]];
 		}
-	}
 	return (NULL);
 }
