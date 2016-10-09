3dccfe5a13efefbc766615e094ebccec75a096ec
Clément Renault <renault.cle@gmail.com>
2016-10-09 16:13:06 +0200

    Cbuffer const for _get functions

--- src/cbuffer/cbuffer_get.c
+++ src/cbuffer/cbuffer_get.c
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/05 19:51:28 by crenault          #+#    #+#             */
-/*   Updated: 2016/10/08 18:29:33 by crenault         ###   ########.fr       */
+/*   Updated: 2016/10/09 16:12:38 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "typedefs_42.h"
 #include "cbuffer_42.h"
 
-void		*cbuffer_get(t_cbuffer *buffer, size_t i)
+void		*cbuffer_get(t_cbuffer const *buffer, size_t i)
 {
 	if (i >= buffer->len || buffer->len == 0)
 		return (NULL);
@@ -24,14 +24,14 @@ void		*cbuffer_get(t_cbuffer *buffer, size_t i)
 	return (CBUFFER_GET(buffer, buffer->start + i));
 }
 
-void		*cbuffer_get_front(t_cbuffer *buffer)
+void		*cbuffer_get_front(t_cbuffer const *buffer)
 {
 	if (buffer->len == 0)
 		return (NULL);
 	return (CBUFFER_GET(buffer, buffer->start));
 }
 
-void		*cbuffer_get_back(t_cbuffer *buffer)
+void		*cbuffer_get_back(t_cbuffer const *buffer)
 {
 	if (buffer->len == 0)
 		return (NULL);
