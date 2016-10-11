c684334ff6c1b23b702bd76b2fe6931574f72081
Theophile VALLEE <tvallee@student.42.fr>
2016-10-11 16:37:34 +0200

    fix coding style

--- src/unistd/ft_getopt.c
+++ src/unistd/ft_getopt.c
@@ -6,7 +6,7 @@
 /*   By: tvallee <tvallee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/30 13:49:50 by tvallee           #+#    #+#             */
-/*   Updated: 2016/10/11 14:53:10 by tvallee          ###   ########.fr       */
+/*   Updated: 2016/10/11 16:36:59 by tvallee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ static int	opt_get_type(char *const av[], const char *optstring, t_opt *state)
 
 	temp = av[state->optind][state->optoff];
 	state->optopt = temp;
-	if (!(addr = ft_strchr(optstring, temp)))
+	if ((addr = ft_strchr(optstring, temp)) == NULL)
 	{
-		if (av[state->optind][state->optoff + 1])
+		if (av[state->optind][state->optoff + 1] != '\0')
 			state->optoff += 1;
 		else
 		{
@@ -42,15 +42,15 @@ static int	parse_operand_opt(int ac, char *const av[], const char *optstring,
 	int c;
 
 	c = av[state->optind][state->optoff];
-	if (!av[state->optind][state->optoff + 1])
+	if (av[state->optind][state->optoff + 1] == '\0')
 	{
 		state->optarg = av[state->optind + 1];
 		state->optind += 2;
 		state->optoff = 0;
 		if (state->optind > ac) {
-			if (state->opterr && *optstring != ':')
+			if (state->opterr && optstring[0] != ':')
 				ft_dprintf(2, GETOPT_MULT_ERR_FORMAT, av[0], c);
-			return ((*optstring == ':') ? ':' : '?');
+			return ((optstring[0] == ':') ? ':' : '?');
 		}
 	}
 	else
@@ -93,7 +93,7 @@ int			ft_getopt(int ac, char *const av[], const char *optstring,
 	{
 		if (state->optind < ac && is_opt_format(av[state->optind]))
 		{
-			if (*(av[state->optind] + 1) == '-' && !*(av[state->optind] + 2))
+			if (av[state->optind][1] == '-' && av[state->optind][2] == '\0')
 				state->optind += 1;
 			else
 			{
