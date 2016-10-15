41dbab774f924380c6cea130f1e01b8a00679b28
Clément Renault <renault.cle@gmail.com>
2016-10-15 19:39:17 +0200

    Listed new features to add to array

--- inc/array_42.h
+++ inc/array_42.h
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 16:28:48 by djean             #+#    #+#             */
-/*   Updated: 2016/10/15 00:33:39 by crenault         ###   ########.fr       */
+/*   Updated: 2016/10/15 19:38:17 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,28 +23,36 @@
 
 # define TARRAY_MIN_SIZE		8
 # define TARRAY_GROWTH_FACTOR	2
-# define TARRAY_NEED_RESIZE(v)	((v)->len + 1 > (v)->capacity)
-# define TARRAY_GET(v, i)		((char*)(v)->data + ((v)->elem_size * (i)))
-# define TARRAY_ADDR_MIN(v)		((v)->data)
-# define TARRAY_ADDR_MAX(v)		((void*)TARRAY_GET(v, (v)->len))
-# define TARRAY_IN_RANGE(v, p)	((p) >= (v)->data && (p) < TARRAY_ADDR_MAX(v))
+# define TARRAY_NEED_RESIZE(v)	((v)->len + 1 > (v)->capacity) // TODO delete
+# define TARRAY_GET(v, i)		((char*)(v)->data + ((v)->elem_size * (i))) // TODO delete
+# define TARRAY_ADDR_MIN(v)		((v)->data) // TODO delete
+# define TARRAY_ADDR_MAX(v)		((void*)TARRAY_GET(v, (v)->len)) // TODO delete
+# define TARRAY_IN_RANGE(v, p)	((p) >= (v)->data && (p) < TARRAY_ADDR_MAX(v)) // TODO delete ??
 
-t_array		*array_init(t_array *v, size_t size, size_t elem_size);
-void		array_destroy(t_array *v);
+t_array		*array_create(size_t elem_size); // TODO
+t_array		*array_create_with_capacity(size_t capacity, size_t elem_size); // TODO
+void		array_destroy(t_array *a); // TODO
+
+t_array		*array_init(t_array *a, size_t elem_size); // TODO
+t_array		*array_init_with_capacity(t_array *a, size_t capacity, size_t elem_size); // TODO
+t_array		*array_shutdown(t_array *a); // TODO
+
+t_array		*array_reserve(t_array *a, size_t additional); // TODO
+t_array		*array_shrink_to_fit(t_array *a); // TODO
+ssize_t		array_truncate(t_array *a, size_t n); // TODO
+
 t_array		*array_copy(t_array *dst, const t_array *src);
-
-void		*array_resize(t_array *v);
 
 void		*array_get_at(t_array *v, size_t i);
 void		array_set_at(t_array *v, size_t i, const void *e);
-int			array_indexof(t_array *v, void *e); // TODO better with a search_func
+// TODO better with a search_func, rework this name/function
+int			array_indexof(t_array *v, void *e);
 
-void		*array_create_node(t_array *v);
+void		*array_create_node(t_array *v); // TODO delete this
 
 t_array		*array_insert_at(t_array *v, size_t i, const void *e);
 void		*array_replace_at(t_array *v, size_t i, const void *e, void *old);
 
-// TODO diff between these two functions
 void		*array_remove_at(t_array *v, size_t i, void *removed);
 void		*array_remove_elem(t_array *v, void *e);
 
