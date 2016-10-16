c3016990a4ac38fd11427d15b5f05ee62a9a9d69
Clément Renault <renault.cle@gmail.com>
2016-10-16 16:23:01 +0200

    fixed string_reserve to handle '\0'

--- src/string/string_cat.c
+++ src/string/string_cat.c
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 16:30:57 by djean             #+#    #+#             */
-/*   Updated: 2016/10/15 15:21:52 by crenault         ###   ########.fr       */
+/*   Updated: 2016/10/16 16:20:07 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_string	*string_cat(t_string *s, const char *str)
 
 t_string	*string_ncat(t_string *s, const char *str, size_t len)
 {
-	if (string_reserve(s, len + 1) == NULL)
+	if (string_reserve(s, len) == NULL)
 		return (NULL);
 	ft_memcpy(s->str + s->len, str, len);
 	s->len += len;
--- src/string/string_reserve.c
+++ src/string/string_reserve.c
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/13 19:19:08 by crenault          #+#    #+#             */
-/*   Updated: 2016/10/14 23:59:32 by crenault         ###   ########.fr       */
+/*   Updated: 2016/10/16 16:16:22 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 t_string	*string_reserve(t_string *s, size_t additional)
 {
+	additional += 1;
 	if (s->capacity - s->len < additional)
 	{
 		if (IS_POWER_OF_2(s->len + additional))
