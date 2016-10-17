80988ba9882fc16dd8d391224aad9d96da83abb5
Kerollmops <renault.cle@gmail.com>
2016-10-17 12:47:06 +0200

    removed many code duplication, added clarity

--- src/array/array_create.c
+++ src/array/array_create.c
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 16:31:25 by djean             #+#    #+#             */
-/*   Updated: 2016/10/17 12:06:33 by crenault         ###   ########.fr       */
+/*   Updated: 2016/10/17 12:45:53 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,7 @@
 
 t_array			*array_create(size_t elem_size)
 {
-	t_array		*a;
-
-	a = malloc(sizeof(t_array));
-	if (a == NULL)
-		return (NULL);
-	if (array_init(a, elem_size) == NULL)
-	{
-		free(a);
-		return (NULL);
-	}
-	return (a);
+	return (array_create_with_capacity(elem_size, TARRAY_INIT_SIZE));
 }
 
 t_array			*array_create_with_capacity(size_t elem_size, size_t capacity)
