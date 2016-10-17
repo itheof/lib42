236ff0d855b6af9f105333f20db10aa14e19078b
Clément Renault <renault.cle@gmail.com>
2016-10-17 17:05:46 +0200

    string: string_set fixed

--- src/string/string_replace.c
+++ src/string/string_replace.c
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/10 16:30:58 by djean             #+#    #+#             */
-/*   Updated: 2016/10/17 17:02:08 by crenault         ###   ########.fr       */
+/*   Updated: 2016/10/17 17:03:52 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_string	*string_set(t_string *s, size_t from, size_t n, int c)
 {
 	if (from > s->len)
 		return (NULL);
-	if (string_reserve(s, n - from) == NULL)
+	if (string_reserve(s, n) == NULL)
 		return (NULL);
 	ft_memset(s->str + from, c, n);
 	if (from + n > s->len)
