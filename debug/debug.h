d28af6b76483362a31addab374c224b5984fd140
Darius Kramer <dev@dkramer.fastmail.com>
2016-11-21 12:49:45 +0100

    debug: use of switch instead of if/else

--- debug/debug.h
+++ debug/debug.h
@@ -2,31 +2,43 @@
 # define DEBUG_H
 
 #include <stdio.h>
-#define DEBUG_ARRAY_PRINT_STR	(0)
-#define DEBUG_ARRAY_PRINT_PTR	(1)
+#define DEBUG_ARRAY_PRINT_STR		(0)
+#define DEBUG_ARRAY_PRINT_PTR		(1)
+#define DEBUG_ARRAY_PRINT_USG		(2)
+#define DEBUG_ARRAY_PRINT_SIZET		(3)
 
-#define debug_array_print(a, p)										\
-	do {															\
-		printf("array->capacity: %zu\n", (a)->capacity);			\
-		printf("array->len: %zu\n", (a)->len);						\
-		if ((p) == DEBUG_ARRAY_PRINT_STR)							\
-		{															\
-			for (size_t i = 0; i < (a)->capacity; ++i)				\
-			printf("a->data[%zu] => %s\n", i, array_get_at(a, i));	\
-		}															\
-		else if ((p) == (void(*)(void*))DEBUG_ARRAY_PRINT_PTR)		\
-		{															\
-			for (size_t i = 0; i < (a)->capacity; ++i)				\
-			printf("a->data[%zu] => %p\n", i, array_get_at(a, i));	\
-		}															\
-		else														\
-		{															\
-			for (size_t i = 0; i < (a)->capacity; ++i)				\
-			{														\
-				printf("a->data[%zu] => ", i);						\
-				(*(void(*)(void*))p)(array_get_at(a, i));			\
-			}														\
-		}															\
-	} while (0);													\
+#define debug_array_print(a, p)												\
+	do {																	\
+		printf("array->capacity: %zu\n", (a)->capacity);					\
+		printf("array->len: %zu\n", (a)->len);								\
+		switch (p) {														\
+			case DEBUG_ARRAY_PRINT_STR:										\
+				for (size_t i = 0; i < (a)->len; ++i)						\
+					printf("a->data[%zu] => %s\n", i, *(char**)array_get_at(a, i));	\
+				break ;														\
+																			\
+			case DEBUG_ARRAY_PRINT_PTR:										\
+			for (size_t i = 0; i < (a)->len; ++i)							\
+			printf("a->data[%zu] => %p\n", i, *(void**)array_get_at(a, i));	\
+				break ;														\
+																			\
+			case DEBUG_ARRAY_PRINT_USG:										\
+				for (size_t i = 0; i < (a)->len; ++i)						\
+					printf("a->data[%zu] => %u\n", i, *(unsigned*)array_get_at(a, i));	\
+				break ;														\
+																			\
+			case DEBUG_ARRAY_PRINT_SIZET:									\
+				for (size_t i = 0; i < (a)->len; ++i)						\
+					printf("a->data[%zu] => %zu\n", i, *(size_t*)array_get_at(a, i));	\
+				break ;														\
+																			\
+			default:														\
+				for (size_t i = 0; i < (a)->len; ++i)						\
+				{															\
+					printf("a->data[%zu] => ", i);							\
+					(*(void(*)(void*))p)(*(void**)array_get_at(a, i));		\
+				}															\
+		}																	\
+	} while (0);															\
 
 #endif
