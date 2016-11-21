3f5ecf3748af936d00da45ca91cbf59dffda1a14
Darius Kramer <dev@dkramer.fastmail.com>
2016-11-21 11:32:52 +0100

    debug: redesign debug_array_print to use macro

--- debug/debug.h
+++ debug/debug.h
@@ -1,21 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   debug.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: djean <djean@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/05/13 17:11:48 by djean             #+#    #+#             */
-/*   Updated: 2016/05/13 17:12:40 by djean            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef DEBUG_H
 # define DEBUG_H
 
-# include <string.h>
-# include "ft_vector.h"
+#include <stdio.h>
+#define DEBUG_ARRAY_PRINT_STR	(0)
+#define DEBUG_ARRAY_PRINT_PTR	(1)
 
-void	ft_vector_print(t_vector *v, void (*print)(void*));
+#define debug_array_print(a, p)										\
+	do {															\
+		printf("array->capacity: %zu\n", (a)->capacity);			\
+		printf("array->len: %zu\n", (a)->len);						\
+		if ((p) == DEBUG_ARRAY_PRINT_STR)							\
+		{															\
+			for (size_t i = 0; i < (a)->capacity; ++i)				\
+			printf("a->data[%zu] => %s\n", i, array_get_at(a, i));	\
+		}															\
+		else if ((p) == (void(*)(void*))DEBUG_ARRAY_PRINT_PTR)		\
+		{															\
+			for (size_t i = 0; i < (a)->capacity; ++i)				\
+			printf("a->data[%zu] => %p\n", i, array_get_at(a, i));	\
+		}															\
+		else														\
+		{															\
+			for (size_t i = 0; i < (a)->capacity; ++i)				\
+			{														\
+				printf("a->data[%zu] => ", i);						\
+				(*(void(*)(void*))p)(array_get_at(a, i));			\
+			}														\
+		}															\
+	} while (0);													\
 
 #endif
