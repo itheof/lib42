94e38dfe96b339cbdbb526bfbeb356453fc9e38a
Theophile VALLEE <tvallee@student.42.fr>
2016-11-28 17:55:07 +0100

    adding kill prototype declaration

--- src/stdlib/ft_abort.c
+++ src/stdlib/ft_abort.c
@@ -6,15 +6,16 @@
 /*   By: tvallee <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 16:13:03 by tvallee           #+#    #+#             */
-/*   Updated: 2016/11/28 16:33:04 by tvallee          ###   ########.fr       */
+/*   Updated: 2016/11/28 17:54:44 by tvallee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdlib_42.h"
-#include <signal.h>
 #ifdef __linux__
 # include <sys/types.h>
+# define _POSIX_C_SOURCE
 #endif
+#include <signal.h>
 
 void	ft_abort(void)
 {
