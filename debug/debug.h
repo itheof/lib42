d52015e21e7d690ec975ff6485f560046db2cb50
Darius Kramer <dev@dkramer.fastmail.com>
2017-02-25 19:10:53 +0100

    debug: add PRINT_NONE to array_debug_print

--- debug/debug.h
+++ debug/debug.h
@@ -6,11 +6,14 @@
 #define DEBUG_ARRAY_PRINT_PTR		(1)
 #define DEBUG_ARRAY_PRINT_USG		(2)
 #define DEBUG_ARRAY_PRINT_SIZET		(3)
+#define DEBUG_ARRAY_PRINT_NONE		(4)
 
 #define debug_array_print(a, p)												\
 	do {																	\
 		printf("array->capacity: %zu\n", (a)->capacity);					\
 		printf("array->len: %zu\n", (a)->len);								\
+		printf("array->elem_size: %zu\n", (a)->elem_size);					\
+		printf("array->data: %p\n", (void*)(a)->data);						\
 		switch (p) {														\
 			case DEBUG_ARRAY_PRINT_STR:										\
 				for (size_t i = 0; i < (a)->len; ++i)						\
@@ -30,6 +33,9 @@
 			case DEBUG_ARRAY_PRINT_SIZET:									\
 				for (size_t i = 0; i < (a)->len; ++i)						\
 					printf("a->data[%zu] => %zu\n", i, *(size_t*)array_get_at(a, i));	\
+				break ;														\
+																			\
+			case DEBUG_ARRAY_PRINT_NONE:									\
 				break ;														\
 																			\
 			default:														\
